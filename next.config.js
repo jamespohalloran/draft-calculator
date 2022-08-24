25ed67e0c96b32880de216eb678de1fdcd7857f6
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-24 20:17:20 -0300

    move players

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   reactStrictMode: true,
   swcMinify: true,
 };
