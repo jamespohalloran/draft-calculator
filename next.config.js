0ba2a540f3b2310527cde5e04f6fa59d5cd51d2b
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-24 19:12:06 -0300

    next tailwind init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
