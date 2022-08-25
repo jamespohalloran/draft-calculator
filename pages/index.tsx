ee988defc006a48e06d43d312027404ff025b4fd
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-25 19:36:00 -0300

    btn styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ const PlayerModal = observer(({ race }: { race: Race }) => {
             </button>
             <div className="modal-action">
               <label htmlFor="my-modal" className="btn">
-                Start Race
+                Done
               </label>
             </div>
           </div>
@@ -122,9 +122,6 @@ const RaceTrack = observer(
   ({ race, running }: { race: Race; running: boolean }) => {
     return (
       <>
-        <label htmlFor="my-modal" className="btn modal-button">
-          Race Settings
-        </label>
         {/* Put this part before </body> tag */}
         <input type="checkbox" id="my-modal" className="modal-toggle" />
         <PlayerModal race={race} />
@@ -210,14 +207,40 @@ const Home: NextPage = () => {
             The League - Draft randomizer
           </h1>
           {!raceStarted && (
-            <button
-              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-              onClick={() => {
-                setRaceStarted(true);
-              }}
-            >
-              Start race
-            </button>
+            <div className="space-x-2">
+              <button
+                className="btn align-top"
+                onClick={() => {
+                  setRaceStarted(true);
+                }}
+              >
+                Start race
+              </button>
+              <label
+                htmlFor="my-modal"
+                className="btn btn-outline modal-button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </label>
+            </div>
           )}
         </div>
 
