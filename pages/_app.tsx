6c17eb20dda88ea049db8e413201ef6c6bbbae14
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-25 23:19:54 -0300

    SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="DraftOrders"
+        titleTemplate="DraftOrders"
+        defaultTitle="DraftOrders"
+        description="Generate your draft order, with a little fanfare."
+        canonical="https://www.draftorders.com/"
+        openGraph={{
+          url: "https://www.draftorders.com/",
+          title: "DraftOrders.com",
+          description: "Generate your draft order, with a little fanfare.",
+          images: [
+            {
+              url: "/screenshot.png",
+              width: 2222,
+              height: 1196,
+              alt: "DraftOrders.com",
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
