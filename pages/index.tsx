8a3e7edfc14e8551184b23fb2cb05c74a4a4f0c4
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-25 22:04:24 -0300

    light styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -134,7 +134,7 @@ const PlayerModal = observer(({ race }: { race: Race }) => {
                   race.racers.splice(index, 1);
                   cacheManager.updateCache();
                 }}
-                className="ml-2 flex-none btn btn-square btn-outline"
+                className="ml-2 flex-none btn btn-squar btn btn-outline"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -159,12 +159,12 @@ const PlayerModal = observer(({ race }: { race: Race }) => {
                 race.racers = [...race.racers, new Racer("")];
                 cacheManager.updateCache();
               }}
-              className="place-self-center text-white btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+              className="place-self-center text-white btn btn-outline"
             >
               Add New User
             </button>
             <div className="modal-action">
-              <label htmlFor="my-modal" className="text-white btn">
+              <label htmlFor="my-modal" className="text-white btn btn-outline">
                 Done
               </label>
             </div>
@@ -270,20 +270,29 @@ const Home: NextPage = () => {
   return (
     <div className="w-screen min-h-screen bg-green-800">
       <main>
-        <div className="p-2">
-          <h1 className="font-medium leading-tight text-2xl text-black-600">
-            The League - Draft randomizer
+        <div className="p-2 flex bg-neutral">
+          <h1 className="self-center flex-none font-medium leading-tight text-2xl text-black-600 align-middle">
+            Draft randomizer
           </h1>
+          <div className="self-center flex-none ml-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12c0-1.232.046-2.453.138-3.662a4.006 4.006 0 013.7-3.7 48.678 48.678 0 017.324 0 4.006 4.006 0 013.7 3.7c.017.22.032.441.046.662M4.5 12l-3-3m3 3l3-3m12 3c0 1.232-.046 2.453-.138 3.662a4.006 4.006 0 01-3.7 3.7 48.657 48.657 0 01-7.324 0 4.006 4.006 0 01-3.7-3.7c-.017-.22-.032-.441-.046-.662M19.5 12l-3 3m3-3l3 3"
+              />
+            </svg>
+          </div>
+          <div className="flex-auto"></div>
           {!raceStarted && (
-            <div className="space-x-2 mt-4">
-              <button
-                className="btn align-top text-white"
-                onClick={() => {
-                  setRaceStarted(true);
-                }}
-              >
-                Start race
-              </button>
+            <div className="flex-none space-x-2">
               <label
                 htmlFor="my-modal"
                 className="btn btn-outline modal-button text-white"
@@ -308,12 +317,52 @@ const Home: NextPage = () => {
                   />
                 </svg>
               </label>
+              <button
+                className="btn btn-outline align-top text-white"
+                onClick={() => {
+                  setRaceStarted(true);
+                }}
+              >
+                Start race
+              </button>
             </div>
           )}
         </div>
 
         <RaceTrack race={race} running={raceStarted} />
       </main>
+      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+        <div className="items-center grid-flow-col">
+          <svg
+            width={36}
+            height={36}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            className="fill-current"
+          >
+            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z" />
+          </svg>
+          <p>
+            Developed by <a href="http://johalloran.dev">James OHalloran</a>
+          </p>
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+        <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+          <a href="https://twitter.com/jamespohalloran">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              className="fill-current"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+            </svg>
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
