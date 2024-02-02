8a2225078d53b6d72c93d737b008fcea8f9f7547
James O'Halloran <james.p.ohalloran@gmail.com>
2024-02-02 14:05:22 -0400

    instantiate players

---
+++ pebble/prefabs/gameManager.ts
@@ -0,0 +1,59 @@
+import { BaseObject, PebbleScene } from "@pebble-engine/core";
+import Player from "./player";
+
+export default class GameManager extends BaseObject {
+  public constructor({
+    name,
+    threeObj,
+    _id,
+  }: {
+    name: string;
+    _id: string;
+    threeObj: any;
+  }) {
+    super({ name, threeObj, _id });
+  }
+
+  public start(_pebbleScene: PebbleScene): void {
+    const initialPlayerProps = {
+      threeObj: {
+        position: { x: -1, y: 1, z: 3 },
+        rotation: { x: 0, y: 180, z: 0 },
+        scale: { x: 0.25, y: 0.25, z: 0.25 },
+      },
+
+      //   BoxColliderBehaviour: {
+      //     kinetic: false, // maybe change this later, but this collides with new arrows and changes position
+      //     offset: { x: 0, y: 0, z: 0 },
+      //     dimensions: { x: 0.1, y: 0.5, z: 0.1 },
+      //   },
+    } as any;
+
+    //instantiate players at x positions 0-9, with a promise.all
+    const instantiatePlayer = async ({ i }: any) => {
+      // clone initialProps, but replace threeObj.position.x with i
+      const newProps = {
+        ...initialPlayerProps,
+        threeObj: {
+          ...initialPlayerProps.threeObj,
+          position: {
+            ...initialPlayerProps.threeObj.position,
+            x: i - 3,
+          },
+        },
+      };
+      const newObj = new Player(newProps);
+
+      await _pebbleScene.instantiate(newObj);
+      _pebbleScene.scene.add(newObj.threeObj!);
+    };
+
+    Promise.all(
+      Array.from({ length: 10 }, (_, i) => i).map((i) =>
+        instantiatePlayer({ i })
+      )
+    );
+
+    debugger;
+  }
+}
--- pebble/prefabs/index.ts
+++ pebble/prefabs/index.ts
@@ -1,3 +1,4 @@
 export { default as Car } from "./car";
 export { default as Flycam } from "./flycam";
 export { default as Player } from "./player";
+export { default as GameManager } from "./gameManager";
