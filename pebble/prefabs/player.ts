7c727b5081dd0c968122cc6c43b5a8ea2beda724
James O'Halloran <james.p.ohalloran@gmail.com>
2024-02-02 21:56:08 -0400

    place label

--- pebble/prefabs/player.ts
+++ pebble/prefabs/player.ts
@@ -25,6 +25,7 @@ export default class Player extends BaseReactiveObject {
 
   private clock: THREE.Clock;
   private label: Text;
+  private placeLabel: Text;
 
   private _running = false;
 
@@ -68,6 +69,7 @@ export default class Player extends BaseReactiveObject {
       this.complete = true;
       this.threeObj!.position.z = -3;
       this.threeObj!.rotation.z = 0;
+      this.showPlaceLabel(3); // TODO - replace with index of player in game manager
     }
   }
 
@@ -75,10 +77,54 @@ export default class Player extends BaseReactiveObject {
     return this.threeObj!.position.z <= -3;
   };
 
+  private showPlaceLabel = (place: number) => {
+    // generate string from place. E.g 1 -> 1st, 2 -> 2nd, 3 -> 3rd, 4 -> 4th, etc.
+    let placeString = place.toString();
+    if (placeString.length > 1) {
+      const lastDigit = placeString.charAt(placeString.length - 1);
+      if (lastDigit === "1") {
+        placeString += "st";
+      } else if (lastDigit === "2") {
+        placeString += "nd";
+      } else if (lastDigit === "3") {
+        placeString += "rd";
+      } else {
+        placeString += "th";
+      }
+    } else {
+      if (placeString === "1") {
+        placeString += "st";
+      } else if (placeString === "2") {
+        placeString += "nd";
+      } else if (placeString === "3") {
+        placeString += "rd";
+      } else {
+        placeString += "th";
+      }
+    }
+
+    this.placeLabel.text = placeString;
+    this.placeLabel.visible = true;
+  };
+
   protected override async getThreeObject() {
     try {
       const prefab = await this.loadPrefab();
       const newobj = SkeletonUtils.clone(prefab.scene);
+
+      this.placeLabel = new Text();
+      this.placeLabel.visible = false;
+      this.placeLabel.rotation.y = -Math.PI / 2;
+      this.placeLabel.text = "";
+      this.placeLabel.fontSize = 0.7;
+      this.placeLabel.fontWeight = "bold";
+      this.placeLabel.anchorX = "center";
+      this.placeLabel.color = 0x000000;
+      this.placeLabel.position.x = 0;
+      this.placeLabel.position.y = 3;
+      this.placeLabel.position.z = 0;
+      this.placeLabel.sync();
+      newobj!.add(this.placeLabel);
 
       this.label = new Text();
       this.label.rotation.y = -Math.PI / 2;
