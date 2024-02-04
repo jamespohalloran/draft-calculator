9a176186720219da17af715bfc14dcea72d2dce1
James O'Halloran <james.p.ohalloran@gmail.com>
2024-02-04 14:50:53 -0400

    game cleanup

--- pages/GameUI.tsx
+++ pages/GameUI.tsx
@@ -26,31 +26,9 @@ export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
       <div className="w-screen min-h-screen bg-green-800">
         <main>
           <Header gameManager={gameManager} />
-          {/* <RaceTrack race={race} running={raceStarted} /> */}
           <div className="bg-gradient-to-b from-blue-300 to-blue-600">
-            {/* gradient background */}
             <div ref={threeContainer} className=""></div>
           </div>
-          {/* {gameManager.running && (
-            <div className="m-2">
-              <table className="table-auto table rounded-sm">
-                <thead>
-                  <tr>
-                    <th>Results</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                {race.racers.map((_, i) => (
-                  <tr key={i} className="bg-white text-black outline">
-                    <th>{i + 1}</th>
-                    <td>
-                      {race.results.length > i ? race.results[i].name : ""}
-                    </td>
-                  </tr>
-                ))}
-              </table>
-            </div>
-          )}{" "} */}
         </main>
       </div>
       <input type="checkbox" id="my-modal" className="modal-toggle" />
