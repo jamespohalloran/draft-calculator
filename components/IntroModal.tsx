65ccf94c803809a75a12439eb9ba3b7cd934876a
James O'Halloran <james.p.ohalloran@gmail.com>
2024-02-04 22:23:48 -0400

    intro modal

---
+++ components/IntroModal.tsx
@@ -0,0 +1,44 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+export function IntroModal() {
+  return (
+    <div>
+      <AlertDialog defaultOpen={true}>
+        <AlertDialogContent className="h-[90vh] overflow-scroll">
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              <h1>Draft Orders</h1>
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              {/* welcome message that describes draft orders, a random order generator */}
+              <div className="space-y-2">
+                <h2>
+                  Draft Orders is a tool to help you generate random draft
+                  orders for your fantasy sports league.
+                </h2>
+                <img src="/screenshot.png" />
+                <p>
+                  To get started, click the settings to customize the names, and
+                  click the &quot;Start Race&quot; button.
+                </p>
+              </div>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogAction>Get Started</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+}
--- pages/GameUI.tsx
+++ pages/GameUI.tsx
@@ -4,6 +4,7 @@ import React, { useEffect } from "react";
 import { Header } from "./components/Header";
 import { PlayerModal } from "./components/PlayerModal";
 import { useGameState } from "./utils/gameState";
+import { IntroModal } from "components/IntroModal";
 
 export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
   const gameManager = pebbleScene.objects.find(
@@ -30,6 +31,7 @@ export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
           <div className="bg-gradient-to-b from-blue-300 to-blue-600">
             <div ref={threeContainer} className=""></div>
           </div>
+          <IntroModal />
         </main>
       </div>
     </>
