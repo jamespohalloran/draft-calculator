82bf56aff2dcc9ba5038439a12a46727c4ad6329
James O'Halloran <james.p.ohalloran@gmail.com>
2024-02-05 00:05:28 -0400

    better intro

--- pages/GameUI.tsx
+++ pages/GameUI.tsx
@@ -3,6 +3,21 @@ import { GameManager } from "../pebble/prefabs";
 import React, { useEffect } from "react";
 import { Header } from "./components/Header";
 import { IntroModal } from "components/IntroModal";
+import { useGameState } from "./utils/gameState";
+import { State } from "pebble/prefabs/gameManager";
+import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { PlayerModal } from "./components/PlayerModal";
 
 export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
   const gameManager = pebbleScene.objects.find(
@@ -21,6 +36,8 @@ export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
     };
   }, []);
 
+  const gameState = useGameState(gameManager);
+
   return (
     <>
       <div className="w-screen min-h-screen bg-green-800">
@@ -29,9 +46,85 @@ export const GameUI = ({ pebbleScene }: { pebbleScene: PebbleScene }) => {
           <div className="bg-gradient-to-b from-blue-300 to-blue-600">
             <div ref={threeContainer} className=""></div>
           </div>
-          <IntroModal />
+          <PlayerModalDialogue gameManager={gameManager}>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-x-2">
+              {gameState.state === State.intro && (
+                <>
+                  <AlertDialogTrigger asChild>
+                    <Button
+                      className="btn btn-outline align-top"
+                      variant={"secondary"}
+                    >
+                      <span className="pr-1">Configure Players{"  "}</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                        />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                        />
+                      </svg>
+                    </Button>
+                  </AlertDialogTrigger>
+                  <Button
+                    className="btn btn-outline align-top text-white"
+                    onClick={() => {
+                      gameManager.startRace();
+                    }}
+                  >
+                    Start race
+                  </Button>
+                </>
+              )}
+            </div>
+          </PlayerModalDialogue>
         </main>
       </div>
     </>
   );
 };
+
+function PlayerModalDialogue({
+  gameManager,
+  children,
+}: {
+  gameManager: GameManager;
+  children: React.ReactNode;
+}) {
+  return (
+    <AlertDialog
+      onOpenChange={(open) => {
+        if (open) {
+          gameManager.setState(State.playerSelect);
+        } else {
+          gameManager.setState(State.intro);
+        }
+      }}
+    >
+      <AlertDialogContent className="h-[90vh] overflow-scroll">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Race Settings</AlertDialogTitle>
+          <AlertDialogDescription>
+            <PlayerModal gameManager={gameManager} />
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+      {children}
+    </AlertDialog>
+  );
+}
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -2,20 +2,9 @@ import React from "react";
 import { GameManager } from "../../pebble/prefabs";
 import { useGameState } from "../utils/gameState";
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import { PlayerModal } from "./PlayerModal";
 import { State } from "pebble/prefabs/gameManager";
+import { IntroModal } from "components/IntroModal";
 
 export const Header = ({ gameManager }: { gameManager: GameManager }) => {
   const gameState = useGameState(gameManager);
@@ -43,25 +32,7 @@ export const Header = ({ gameManager }: { gameManager: GameManager }) => {
       <div className="flex-auto"></div>
 
       <div className="flex flex-row space-x-2 [&>*]:m-auto">
-        <a target="_blank" href="/about" className="text-black">
-          <div className=" text-white  cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
-          </div>
-        </a>
-        <PlayerModalDialogue gameManager={gameManager} />
+        <IntroModal />
 
         {gameState.state === State.race ? (
           <Button
@@ -74,75 +45,9 @@ export const Header = ({ gameManager }: { gameManager: GameManager }) => {
             Reset race
           </Button>
         ) : (
-          <Button
-            className="btn btn-outline align-top text-white"
-            onClick={() => {
-              gameManager.startRace();
-            }}
-          >
-            Start race
-          </Button>
+          <></>
         )}
       </div>
     </div>
   );
 };
-
-export function PlayerModalDialogue({
-  gameManager,
-}: {
-  gameManager: GameManager;
-}) {
-  return (
-    <AlertDialog
-      onOpenChange={(open) => {
-        if (open) {
-          gameManager.setState(State.playerSelect);
-        } else {
-          gameManager.setState(State.intro);
-        }
-      }}
-    >
-      <AlertDialogTrigger asChild>
-        <div variant="outline" className="text-black">
-          <label
-            htmlFor="my-modal"
-            className="btn btn-outline modal-button cursor-pointer text-white align-middle"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
-          </label>
-        </div>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="h-[90vh] overflow-scroll">
-        <AlertDialogHeader>
-          <AlertDialogTitle>Race Settings</AlertDialogTitle>
-          <AlertDialogDescription>
-            <PlayerModal gameManager={gameManager} />
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-}
